5da421262c75840c72c22402b4a05aa197159430
root <root@ubuntu.(none)>
2015-07-22 17:24:25 +0800

    init git repository

---
+++ Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := ath6kl-fwlog-record
+LOCAL_MODULE_TAGS := debug eng optional
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_SHARED_LIBRARIES := libc libcutils
+LOCAL_SRC_FILES := ath6kl-fwlog-record.c
+LOCAL_CFLAGS := $(CFLAGS)
+include $(BUILD_EXECUTABLE)
